eeaa414477dc81efc749d16beee9bda413d2e265
jesus <Jesusfernandorios93@gmail.com>
2021-11-29 19:01:58 -0700

    set the primary colors for the app. added babershop logo and integrating react-router-dom for secret and public routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import barbershopLogo from './Images/SampleImage1.png'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Fade it barbershop
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={barbershopLogo} alt="Barbershop Logo"/>
       </header>
+
+        <hr style={{color: "white" , width: 1400}}/>
     </div>
   );
 }
