80d076788f5634bf82ec5adf10c99b8ec9379e87
jesus <Jesusfernandorios93@gmail.com>
2021-12-04 19:37:55 -0700

    added appointment component

---
+++ src/Components/AppointmentButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './Style/appointment_button.css'
+
+export default function AppointmentButton() {
+    return (
+
+        <a href="/" className="bn3637 bn37">Book and Appointment</a>
+    )
+}
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AppointmentButton from './AppointmentButton'
 import BusinessHours from './BusinessHours'
 import Pricing from './Pricing'
 import Safety from './Safety'
@@ -9,6 +10,7 @@ export default function Home() {
         <>
 
         <SocialMedia/>
+        <AppointmentButton/>
         <hr/>
         <BusinessHours/>
         <hr/>
