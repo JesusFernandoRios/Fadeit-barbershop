e73719a50809ae5e84f80425bd186c3aa79654bc
jesus <Jesusfernandorios93@gmail.com>
2021-12-07 19:11:20 -0700

    installed custom gallery component for barber shop images display

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import AppointmentButton from './AppointmentButton'
 import BusinessHours from './BusinessHours'
 import Pricing from './Pricing'
 import Safety from './Safety'
 import SocialMedia from './SocialMedia'
+import ImageGallery from 'react-image-gallery'
 
 export default function Home() {
     return (
