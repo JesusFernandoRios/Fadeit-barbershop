bd7f60cb5223ab45744cf0e2ad77cb204349a5ee
jesus <Jesusfernandorios93@gmail.com>
2021-12-17 12:41:52 -0700

    updated main component added location inormation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
     <Router>
        <header className="App">
           <img className="barber-logo" src={barbershopLogo} alt="Barbershop Logo"/>
+
+          <div className='location_information'>
+            <h2>Location</h2>
+            <p>13610 n 99th ave, Sun City, 85351</p>
+            <p>(623)440-2361</p>
+          </div>
       </header>
 
       <Home/>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -26,7 +26,6 @@ export default function Home() {
     return (
         <>
 
-        {/* <ImageGallery items={images}/> */}
         <hr/>
         <BusinessHours/>
         <hr/>
