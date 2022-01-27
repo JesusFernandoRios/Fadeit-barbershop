ee2dd4ab321366d89332cf8319ebbafe1cf29832
jesus <Jesusfernandorios93@gmail.com>
2022-01-26 21:04:52 -0700

    updating image slides upon customer requests

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,37 +9,20 @@ export default function Home() {
 
     const images = [
         {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/4.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/4.jpg',
+          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image1.jpg',
+          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image1.jpg',
         },
         {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-7.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-7.jpg',
+          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image2.jpg',
+          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image2.jpg',
         },
         {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-8.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-8.jpg',
+          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image3.jpg',
+          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image3.jpg',
         },
         {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-6.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-6.jpg',
-        },
-        {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/2.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/2.jpg',
-        },
-        {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/3.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/3.jpg',
-        },
-        {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/1.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/1.jpg',
-        },
-        {
-          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-1.jpg',
-          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/img-1.jpg',
-          
+          original: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image4.jpg',
+          thumbnail: 'https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Images/new-image4.jpg',
         },
       ];
 
