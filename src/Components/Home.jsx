823cd865a4c8794abb1547a58703c7aa27456871
JesusFernandorRios <Jesusfernandorios93@gmail.com>
2022-05-21 16:31:50 -0700

    added video back to home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -55,7 +55,12 @@ export default function Home() {
           />
           <BusinessHours/>
         </div>
-        
+        <video width="100%" height="700" controls>
+
+          <source src='https://fadeitbarbershop.s3.us-west-2.amazonaws.com/Videos/convertedVideo.mp4' type='video/mp4'/>
+
+          Video file not supported
+        </video>
         <hr/>
         <Pricing />
         <hr/>
